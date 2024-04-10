ba136fb63e785cddf32fc96509215232413c809b
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-04-10 08:40:49 -0400

    just setting things up...

---
+++ clean.py
@@ -0,0 +1,6 @@
+# This script is going to be used to clean my data files. Right now, I'm hoping to have student, teacher, and graduation rate input files that show student diversity and teacher diversity by race and school district and graduation rates by race and school district.
+
+# Importing modules
+import pandas as pd
+
+# 
