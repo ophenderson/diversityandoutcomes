61b0a0ab386da8b9dc5443113cbeaffe1241f44c
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-04-11 08:55:18 -0400

    just drafting out what i need to do for this

---
+++ parity.py
@@ -0,0 +1,4 @@
+# Calculating parity for each racial group
+# Making shp files for each year and racial group, with graduation rates over each county (ex: Black should have a shp file for each year)
+# Showing this graphically: bar graph with parity score on x-axis, graduation rate on y, and each bar representing a different racial group (graphs done by school year)
+# Time series graph showing state student teacher parity over time on one line and state graduation rates on the other (x-axis is school year, y-axis in parity, dots represent parity scores and graduation rates)
