f24aa4ac588c9775f74fac226d1201ff64a23e05
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-04-12 18:41:36 -0400

    diversity

--- clean_grad.py
+++ clean_grad.py
@@ -1,5 +1,20 @@
+# Importing
 import pandas as pd
 import requests
 
-raw = pd.read_excel('Grad Rate 2017-2018.xlsx')
-response = requests.get(raw)
+# Defining a function but IDK
+repo_url = https://github.com/ophenderson/diverstiyandoutcomes.git
+folder_path = 'diverstiyandoutcomes'/2022-2023
+file_name = 'Grad Rate 2022-2023.xlsx'
+def fetch_github_file(repo_url,folder_path,file_name):
+
+# Fetching the file
+raw_file: f"{https://github.com/ophenderson/diverstiyandoutcomes.git}
+# Turning it into a dataframe?
+
+# Dropping unneeded columns - there has to be a way to do this without listing out every column that needs to be dropped
+
+# Grouping by district
+
+
+# Look at the file, does anymore cleaning need to be done?
