76221318552f6ddaae01838811cebdebdfc19b5e
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-04-14 17:24:16 -0400

    Update clean_student.py

--- clean_student.py
+++ clean_student.py
@@ -6,14 +6,10 @@ def fetch_and_clean(repo_owner, repo_name, folder_path, file_name, branch = 'mai
     excel_bytes = response.content
     excel_file = BytesIO(excel_bytes)
     studentdf = pd.read_excel(excel_file)
-    studentdf['BEDSCODE'] = Year # the idea for this was that I would replace the BEDSCODE column with the school year that the data is from, not sure how to do this?
-    graddf.rename(columns={'BEDSCODE':'YEAR'}, inplace=True)
-    keep_cols = ['DISTRICT', 'DISTRICT NAME', 'WHITE MALES', 'WHITE FEMALES', 'WHITE GENDER NOT REPORTED', 'BLACK MALES', 'BLACK FEMALES', 'BLACK GENDER NOT REPORTED','HISPANIC MALES', 'HISPANIC FEMALES', 'HISPANIC GENDER NOT REPORTED')
-    totalw = teacherdf['WHITE MALES', "WHITE FEMALES", "WHITE GENDER NOT REPORTED"].sum(axis=1)
-    totalb = teacherdf['BLACK MALES', "BLACK FEMALES", "BLACK GENDER NOT REPORTED"].sum(axis=1)
-    totalh = teacherdf['HISPANIC MALES', "HISPANIC FEMALES", "HISPANIC GENDER NOT REPORTED"].sum(axis=1)
-    teacherdf = teacherdf[keep_cols]
-    teacherdf.fillna('N/A')
+    studentdf['YEAR'] = YEAR # once again, not sure how to make sure this is done differently for each file
+    keep_cols = ['District', 'District Name', 'Total # Actively Enrolled Students', 'Black or African American', 'Hispanic or Latino', 'White') 
+    studentdf = studentdf[keep_cols]
+    studentdf.fillna('N/A')
   else:
     print(f"Failed to fetch and clean file: {response.status_code} - {response.reason}")
     return None 
