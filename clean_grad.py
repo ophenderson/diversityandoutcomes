659481098f0794f8bb816a51da92f64a4b730977
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-04-21 14:56:59 -0400

    Update clean_grad.py

--- clean_grad.py
+++ clean_grad.py
@@ -1,3 +1,32 @@
+
+# Importing 
+import pandas as pd
+import requests
+
+# Reading in a file
+graddf = pd.read_excel('raw/OVERALL_GRADRATE1718.xlsx')
+
+# Performing cleaning functions
+year = '2017-2018'
+graddf['Year'] = year
+keep_cols = ['DISTRICT', 'SCHOOL', 'GNUMERATOR_RACE_W', 'GDENOM_RACE_W', 'GNUMERATOR_RACE_B', 'GDENOM_RACE_B', 'GNUMERATOR_RACE_H', 'GDENOM_RACE_H']
+graddf = graddf[keep_cols]
+graddf.fillna('N/A')
+
+# These replace functions aren't working
+graddf.replace('-1', '0')
+graddf.replace('-1.0', '0')
+
+
+
+
+
+
+
+
+
+
+
 # Importing
 import pandas as pd
 import requests
@@ -34,8 +63,7 @@ file_content = get(owner, repo, folder_path, file_name)
 if file_content:
     # Now you have the content of the file, you can further process it as needed
     pd.read_excel(file_name)
-#%%
-    
+
     
     
     # Cleaning code
