cfd485c776b065248b7fa8c149176a3766dd93c7
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-05-03 16:06:40 -0400

    starting the join

--- join.py
+++ join.py
@@ -1,6 +1,4 @@
-# This script is going to be used to join (outer join) data for each year into one file so that in the end we have 1 file for each school year that has grad, student, and teacher data
-# This script will also drop special school cases (alternative schools, career and technical schools, etc)
+# This is the joining script. It will take join one of the dataframes on another and then join the remaining dataframe onto the joined dataframe to create 1 file
 
 
-# Want to join grad rate onto teacher data by district id and school and then join that onto student data so that in the end the district and district name data looks like it does in the student data
-# merged = teacher_data.merge(grad_data, on = 'DISTRICT', how ='outer')
+
