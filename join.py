d5540053c93bb52c80bc2a1ace2f1f983e202054
ophenderson <74733541+ophenderson@users.noreply.github.com>
2024-05-04 15:24:05 -0400

    pickle file

--- join.py
+++ join.py
@@ -8,8 +8,6 @@ grads = pd.read_pickle('grad.pkl')
 teachers = pd.read_pickle('teacher.pkl')
 students = pd.read_pickle('student.pkl')
 
-
-
 # Merging teacher and grad dataset by the Year and District columns
 merged = teachers.merge(grads, on=['Year', 'District'], how = 'outer', indicator=True)
 # Looking at the merge indicator to make sure everything says 'Both'
@@ -18,3 +16,10 @@ print(merged['_merge'])
 merged.drop(columns = ['_merge'], inplace=True)
 # Merging student data
 merged = merged.merge(students, on=['Year', 'District'], how = 'outer', indicator=True)
+# Looking at the merge indicator to make sure everything says 'Both'
+print(merged['_merge'])
+# Merge indicator looks good
+merged.drop(columns = ['_merge'], inplace=True)
+
+# Saving to a pickle file
+merged.to_pickle('merged.pkl')
--- parity_index.py
+++ parity_index.py
@@ -2,3 +2,15 @@
 # Making shp files for each year and racial group, with graduation rates over each county (ex: Black should have a shp file for each year)
 # Showing this graphically: bar graph with parity score on x-axis, graduation rate on y, and each bar representing a different racial group (graphs done by school year)
 # Time series graph showing state student teacher parity over time on one line and state graduation rates on the other (x-axis is school year, y-axis in parity, dots represent parity scores and graduation rates)
+
+
+# Importing
+import pandas as pd 
+
+# Reading pickle
+grads = pd.read_pickle('grad.pkl')
+teachers = pd.read_pickle('teacher.pkl')
+students = pd.read_pickle('student.pkl')
+merged_df = pd.read_pickle('merged.pkl')
+
+
